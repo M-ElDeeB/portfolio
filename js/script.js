17b398917fb561a5ae8466529744f00b467f5130
mohamed <you@example.com>
2022-05-25 22:00:57 +0200

    upgrad portfolio v.02

--- js/script.js
+++ js/script.js
@@ -1,94 +1,129 @@
 $(function () {
+  ////welcome page //////////
+  $("#typewriteText").typewrite({
+    actions: [
+      {
+        delay: 1500,
+      },
 
-    ////welcome page //////////
-    $('#typewriteText').typewrite({
+      {
+        type: "Weclome To My Portoflio",
+      },
 
-        actions: [{
-                delay: 1500
-            },
-            {
-                type: 'Hello '
-            },
-            {
-                type: '<br>'
-            },
-            {
-                type: 'Weclome To My Portoflio'
-            },
+      {
+        select: {
+          from: 23,
+          to: 32,
+        },
+      },
+    ],
+  });
 
-            {
-                select: {
-                    from: 23,
-                    to: 32
-                }
-            },
-        ]
+  /////////////////////popup contact//////
+  //menu toggle
+  $(".menu i").click(function () {
+    $(".contact-page").animate(
+      {
+        height: "100vh",
+      },
+      500
+    );
+  });
+
+  //close popup contact page
+  $(".contact-page .menu i").click(function () {
+    $(this).parents(".contact-page").animate(
+      {
+        height: 0,
+      },
+      500
+    );
+  });
+
+  //envelop contact
+  $("header .contact").click(function (e) {
+    e.preventDefault();
+    $(".contact-page").animate(
+      {
+        height: "100vh",
+      },
+      500
+    );
+  });
+
+  $(".about-me .content p:last-child a").click(function (e) {
+    e.preventDefault();
+    $(".contact-page").animate(
+      {
+        height: "100vh",
+      },
+      500
+    );
+  });
+  ////////////////popoup contact///////////
+
+  //////up button//////////
+  $(window).scroll(function () {
+    if ($(this).scrollTop() > 700) {
+      $(".top-button").fadeIn(600);
+    } else {
+      $(".top-button").fadeOut(500);
+    }
+  });
+
+  $(".top-button").click(function () {
+    $("html, body").animate(
+      {
+        scrollTop: 0,
+      },
+      600
+    );
+  });
+
+  $(".explore").on("click", function () {
+    $("html,body").animate({
+      scrollTop: $("header").height(),
     });
-
-
-
-    /////////////////////popup contact//////
-    //menu toggle 
-    $('.menu i').click(function () {
-        $('.contact-page').animate({
-            height: '100vh'
-        }, 500)
-    });
-
-    //close popup contact page
-    $('.contact-page .menu i').click(function () {
-        $(this).parents('.contact-page').animate({
-            height: 0
-        }, 500)
-    });
-
-    //envelop contact
-    $('header .contact').click(function (e) {
-        e.preventDefault();
-        $('.contact-page').animate({
-            height: '100vh'
-        }, 500)
-    });
-
-    $('.about-me .content p:last-child a').click(function (e) {
-        e.preventDefault();
-        $('.contact-page').animate({
-            height: '100vh'
-        }, 500)
-    });
-    ////////////////popoup contact///////////
-
-
-    //////up button//////////
-    $(window).scroll(function () {
-        if ($(this).scrollTop() > 700) {
-            $('.top-button').fadeIn(600)
-        } else {
-            $('.top-button').fadeOut(500)
-
-        }
-    });
-
-    $('.top-button').click(function () {
-        $('html, body').animate({
-            scrollTop: 0
-        }, 600)
-    });
-
-    $('.explore').on("click", function () {
-        $('html,body').animate({
-            scrollTop: $('header').height()
-        })
-    })
+  });
 });
 
-$(window).on('load', function () {
-    $('.welcome').animate({
-            top: '50%',
-            opacity: '1'
-        }, 1000)
-        .delay(5000).animate({
-            left: '-50%',
-            opacity: '0'
-        })
-})
+$(window).on("load", function () {
+  $(".welcome")
+    .animate(
+      {
+        top: "50%",
+        opacity: "1",
+      },
+      1000
+    )
+    .delay(5000)
+    .animate({
+      left: "-50%",
+      opacity: "0",
+    });
+});
+
+var swiper = new Swiper(".swiper", {
+  slidesPerView: 1,
+  spaceBetween: 10,
+  centeredSlides: true,
+  grabCursor: true,
+  loop: true,
+  autoplay: {
+    delay: 4000,
+  },
+  breakpoints: {
+    640: {
+      slidesPerView: 2,
+      spaceBetween: 20,
+    },
+    768: {
+      slidesPerView: 3,
+      spaceBetween: 40,
+    },
+    1200: {
+      slidesPerView: 3,
+      spaceBetween: 30,
+    },
+  },
+});
